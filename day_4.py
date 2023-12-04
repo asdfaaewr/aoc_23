1024ce56926dd99c40a110fab49f45574a012066
asdfaaewr <phil.seemann@gmail.com>
2023-12-04 21:29:35 +0100

    .

---
+++ day_4.py
@@ -0,0 +1,27 @@
+import re
+from collections import defaultdict
+
+day = 4
+
+with open('in_' + str(day) + '.txt') as file:
+    data = [line.strip() for line in file.readlines()]
+
+t = 0
+d = defaultdict(int)
+
+
+for i, line in enumerate(data):
+    
+    w, a = (set(re.findall('\\d+', x)) for x in line.split(': ')[1].split(' | '))
+
+    d[i+1] += 1
+
+    p = len(a.intersection(w)) 
+    if p >= 1:
+         t += 2**(p-1)
+
+    for idx in range(p):
+        d[2+idx+i] += d[i+1]
+
+print(t)
+print(sum(d.values()))
